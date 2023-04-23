f2ea861d29693c0d31a79ea123c2db35c34b4394
hari <heyhariom24@gmail.com>
2023-04-24 00:42:40 +0530

    export coll

--- server/database/exportCollection.js
+++ server/database/exportCollection.js
@@ -1,5 +1,7 @@
-import { database } from "./connect.js";
+// import { database } from "./connect.js";
 
-//const collection = database.collection(DB_COLL);     //syntax
+// //// const collection = database.collection(DB_COLL); //syntax
 
-export const myColl = database.collection("myColl");
+// console.log(database);
+
+// export const myColl = database.collection("myColl");
