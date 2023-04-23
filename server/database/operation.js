254b7ab018d82ca9657d03b97e3a8651769d0b28
hari <heyhariom24@gmail.com>
2023-04-24 00:40:03 +0530

    commit

--- server/database/operation.js
+++
@@ -1,8 +0,0 @@
-// const insertManyResult = await collection.insertMany(recipes);
-
-import { recipes } from "../data.js";
-import { myColl } from "./exportCollection.js";
-
-const insertManyResult = await myColl.insertMany(recipes);
-
-console.log("insert result=", insertManyResult);
