b1068f81b2868721b774248e0084f84a7b281219
hari <heyhariom24@gmail.com>
2023-04-24 00:41:54 +0530

    homecontroller

--- server/controller/homeController.js
+++ server/controller/homeController.js
@@ -1,13 +1,40 @@
-import { s3Uploadsv2 } from "../aws/storage";
+console.log("controller- homeController");
 
-export async function home(req, res) {
-  try {
-    // const file = req.file[0]
-    let resp = await s3Uploadsv2(file);
+const { recipes } = require("../data");
+const { getDb } = require("../database/connect");
 
-    let response = { result: "hello from controller" };
-    res.status(200).json(response);
-  } catch (error) {
-    console.log(error);
-  }
-}
+module.exports = {
+  home: async function home(req, res) {
+    try {
+      let response = { result: "hello from controller" };
+      res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  insertData: async function insertData(req, res, next) {
+    try {
+      /**get connection instance */
+      let database = getDb();
+      // console.log("db_conn", database);
+
+      /**enter collection name into mongodb instance */
+      const myColl = database.collection("myColl");
+      // // console.log("myColl", myColl);
+
+      /**execute operation using db method */
+      let insertManyResult = await myColl.insertMany(recipes);
+
+      // /**in one line (same thing as above but in one line) starts here */
+      // let insertManyResult = await database
+      //   .collection("myColl")
+      //   .insertMany(recipes);
+      // /**in online line ends here */
+
+      let result = insertManyResult;
+      res.status(200).json({ result });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
