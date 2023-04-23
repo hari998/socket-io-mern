99396f780b9e3a85fe559036ccd2c474470673d0
hari <heyhariom24@gmail.com>
2023-04-24 00:38:53 +0530

    export env

---
+++ server/exportEnv.js
@@ -0,0 +1,7 @@
+console.log("exportEnv-");
+
+// export const PORT = process.env.PORT;
+// export const NODE_ENV = process.env.NODE_ENV;
+
+// export const DB_URL = process.env.DB_URL;
+// export const DB_NAME = process.env.DB_NAME;
