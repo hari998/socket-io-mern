47b173f92dc85090a3cda6224da508ca5b290477
hari <heyhariom24@gmail.com>
2023-04-24 00:35:58 +0530

    data

--- server/data.js
+++ server/data.js
@@ -1,44 +1,8 @@
-export const recipes = [
-  {
-    name: "elotes",
-    ingredients: ["corn", "mayonnaise", "cotija cheese", "sour cream", "lime"],
-    prepTimeInMinutes: 35,
-  },
-  {
-    name: "loco moco",
-    ingredients: [
-      "ground beef",
-      "butter",
-      "onion",
-      "egg",
-      "bread bun",
-      "mushrooms",
-    ],
-    prepTimeInMinutes: 54,
-  },
-  {
-    name: "patatas bravas",
-    ingredients: [
-      "potato",
-      "tomato",
-      "olive oil",
-      "onion",
-      "garlic",
-      "paprika",
-    ],
-    prepTimeInMinutes: 80,
-  },
-  {
-    name: "fried rice",
-    ingredients: [
-      "rice",
-      "soy sauce",
-      "egg",
-      "onion",
-      "pea",
-      "carrot",
-      "sesame oil",
-    ],
-    prepTimeInMinutes: 40,
-  },
+console.log("data-");
+
+const recipes = [
+  { title: "Dangal", rating: "Not Rated" },
+  { title: "The Boss Baby", rating: "PG" },
 ];
+
+module.exports = { recipes };
