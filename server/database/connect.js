a46530915fbb218ce2481a595dc340b3c719dc54
hari <heyhariom24@gmail.com>
2023-04-24 00:42:07 +0530

    db connect

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,14 +1,10 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
-// import { DB_NAME, DB_URL } from "../environment/exportEnv.js";
+console.log("database- connect");
 
-import * as dotenv from "dotenv";
-dotenv.config({ path: "../environment/config.env", debug: true });
-// dotenv.config({ path: "../config.env", debug: true });
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const DB_URL = process.env.DB_URL;
-const DB_NAME = process.env.DB_NAME;
+const dbName = process.env.DB_NAME;
+const Db = process.env.ATLAS_URI;
 
-/**Create a MongoClient with a MongoClientOptions object to set the Stable API version */
 //options
 let options = {
   serverApi: {
@@ -17,49 +13,50 @@ let options = {
     deprecationErrors: true,
   },
 };
+const client = new MongoClient(Db, options);
 
-// The MongoClient is the object that references the connection to our
-// datastore (Atlas, for example)
-const client = new MongoClient(DB_URL, options);
+let _conn;
 
-// The connect() method does not attempt a connection; instead it instructs
-// the driver to connect using the settings provided when a connection
-// is required.
-const conn = await client.connect();
-// console.dir(conn, { depth: null }); //when this is logged, "replicaSet" field comes
+module.exports = {
+  connectToServer: async function connectToServer() {
+    try {
+      // console.log("inside---");
 
-// Provide the name of the database and collection you want to use.
-// If the database and/or collection do not exist, the driver and Atlas
-// will create them automatically when you first write data.
-const dbName = DB_NAME;
+      /**using promise start here */
+      // // Connect to database
+      // client
+      //   .connect()
+      //   .then((conn) => {
+      //     console.log("Connected Successfully!", conn);
 
-// Create references to the database and collection in order to run
-// operations on them.
-export const database = client.db(dbName);
-// export const database = new MongoClient(DB_URL, options).connect().db(dbName); //connect is not a function
+      //     // //Close the database connection
+      //     // console.log("Exiting..");
+      //     // client.close();
+      //   })
+      //   .catch((error) => console.log("Failed to connect!", error));
+      /**using promise ends here */
 
-// const collection = database.collection(collectionName);
+      /**using await start here */
+      let conn = await client.connect();
+      // console.log("Connected Successfully!", conn);
 
-console.log(`MongoDB connected-`, database);
+      _conn = conn.db(dbName); //gives connection instance with database name otherwise "admin" db will be connected
+      console.log("Db Connected!");
 
-// export async function connectDB(DB_URL, DB_NAME) {
-//   //options
-//   let options = {
-//     serverApi: {
-//       version: ServerApiVersion.v1,
-//       strict: true,
-//       deprecationErrors: true,
-//     },
-//   };
-//   try {
-//     const client = new MongoClient(DB_URL, options);
-//     const conn = await client.connect(); //(ok, this maybe because of await) but if i remove this , then "db connected" print on top .. ?
-//     const dbName = DB_NAME;
-//     const database = client.db(dbName);
-//     console.log(`DB connected-`); //when i removed "database" from console log, see the order in terminal, it printed "db conntd" at the bottom, why ?
-//     // return database
-//   } catch (error) {
-//     console.log("error=");
-//     console.log(error);
-//   }
-// }
+      // //Close the database connection
+      // console.log("Exiting..");
+      // client.close();
+
+      /**using await ends here */
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  getDb: function getDb() {
+    //this returns connection instance created in above "connectToServer" fxn
+    return _conn;
+  },
+};
+
+// module.exports.connectToServer();
