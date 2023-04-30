53326b2399adf31a20dddfb485ec0a7914248678
hari <heyhariom24@gmail.com>
2023-04-30 22:03:35 +0530

    commit

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,32 @@
-console.log("routes- routes");
+// console.log("routes- routes");
 
 const express = require("express");
 const apiRouter = express.Router();
-const { home, insertData } = require("../controller/homeController");
+const { home, apiHome } = require("../controller/homeController");
+const { insertOne, insertMany } = require("../controller/insertController");
+const { deleteAllData } = require("../controller/deleteController");
+const { findAll } = require("../controller/readController");
+const { updateMany } = require("../controller/updateController");
+
+apiRouter.route("/").get(apiHome);
 
 apiRouter.route("/home").get(home);
-apiRouter.route("/insert/data").get(insertData);
+
+//create
+apiRouter.route("/insert/one").get(insertOne);
+apiRouter.route("/insert/many").get(insertMany);
+
+//read (find)
+apiRouter.route("/find/all").get(findAll);
+// // apiRouter.route("/find/data/id").find(findById);
+
+//update
+apiRouter.route("/update/many").get(updateMany);
+// apiRouter.route("/update/data/id").update(updateById);
+
+//delete
+apiRouter.route("/delete/all/data").delete(deleteAllData);
+// apiRouter.route("/delete/data/id").delete(deleteById);
 
 module.exports = apiRouter;
 
